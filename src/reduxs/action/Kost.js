3a464a02addf3751c9ba3f7b9f9a456b7d358c20
epulskyyy <saepulstr@.com>
2020-04-26 13:25:00 +0700

    ketiga

--- src/reduxs/action/Kost.js
+++ src/reduxs/action/Kost.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
 import { FETCHING_KOST } from '../actionTypes';
+import axios from '../../axios';
 
-export const fetchKostdata = () => {
+export const fetchKostdata = (category) => {
   return (dispatch) => {
     axios
-      .get('http://10.10.12.79:9876/api/kost')
+      .get(`/api/kost/category/${category}`)
       .then((result) => {
         console.log(result.data);
         dispatch({ type: FETCHING_KOST, data: result.data });
